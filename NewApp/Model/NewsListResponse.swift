588cd87eea9e04bc19cbc3cc49c0c72e9a097147
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-15 21:45:50 +0530

    news list response model added

---
+++ NewApp/Model/NewsListResponse.swift
@@ -0,0 +1,46 @@
+//
+//  NewsListResponse.swift
+//  NewApp
+//
+//  Created by MACBOOK on 15/11/21.
+//
+
+import Foundation
+
+// MARK: - NewsListResponse
+struct NewsListResponse: Codable {
+    let status: String
+    let articles: [Article]
+    let totalResults: Int
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        status = try values.decodeIfPresent(String.self, forKey: .status) ?? DocumentDefaultValues.Empty.string
+        articles = try values.decodeIfPresent([Article].self, forKey: .articles) ?? []
+        totalResults = try values.decodeIfPresent(Int.self, forKey: .totalResults) ?? DocumentDefaultValues.Empty.int
+    }
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let author: String
+    let urlToImage: String
+    let title: String
+    let articleDescription: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case author, urlToImage, title
+        case articleDescription = "description"
+        case url
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        author = try values.decodeIfPresent(String.self, forKey: .author) ?? DocumentDefaultValues.Empty.string
+        urlToImage = try values.decodeIfPresent(String.self, forKey: .urlToImage) ?? DocumentDefaultValues.Empty.string
+        title = try values.decodeIfPresent(String.self, forKey: .title) ?? DocumentDefaultValues.Empty.string
+        articleDescription = try values.decodeIfPresent(String.self, forKey: .articleDescription) ?? DocumentDefaultValues.Empty.string
+        url = try values.decodeIfPresent(String.self, forKey: .url) ?? DocumentDefaultValues.Empty.string
+    }
+}
