743dfdae9cbefa4841d23a6957107d23c1dfd918
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-15 20:36:45 +0530

    global constant file added

---
+++ NewApp/ProjectHelpers/GlobalConstants.swift
@@ -0,0 +1,39 @@
+//
+//  GlobalConstants.swift
+//  Truck Tracker
+//
+//  Created by MACBOOK on 06/11/21.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - STORYBOARD
+struct STORYBOARD {
+    static let MAIN = UIStoryboard(name: "Main", bundle: nil)
+}
+
+//MARK: - TABLE_VIEW_CELL
+enum TABLE_VIEW_CELL: String {
+    case NewsListCell
+}
+
+//MARK: - MAIN_STORYBOARD
+enum MAIN_STORYBOARD: String {
+    case NewsListVC
+}
+
+//MARK:- AppColors
+struct AppColors{
+    static let LoaderColor =  #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+}
+
+//MARK:- DocumentDefaultValues
+struct DocumentDefaultValues{
+    struct Empty{
+        static let string =  ""
+        static let int =  0
+        static let bool = false
+        static let double = 0.0
+    }
+}
