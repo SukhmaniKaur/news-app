cd38034b9e3969eda47e494858eaaf557b7eaf74
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-15 20:12:37 +0530

    UI table view news list vc

--- NewApp/View/NewsListVC.swift
+++ NewApp/View/NewsListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsListVC.swift
 //  NewApp
 //
 //  Created by MACBOOK on 15/11/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsListVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
