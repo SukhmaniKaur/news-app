a525aeac347a382131bb05973b589917e9b458f3
Sukhmani Kaur <sukhmanikaur.appknit@gmail.com>
2021-11-15 20:37:17 +0530

    table view delegate and data source methods added to newsListVC along with cell registration

--- NewApp/View/NewsListVC.swift
+++ NewApp/View/NewsListVC.swift
@@ -12,7 +12,31 @@ class NewsListVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configUI()
+    }
+    
+    //MARK: - configUI
+    private func configUI() {
+        tableView.register(UINib(nibName: TABLE_VIEW_CELL.NewsListCell.rawValue, bundle: nil), forCellReuseIdentifier: TABLE_VIEW_CELL.NewsListCell.rawValue)
     }
 
+}
+
+//MARK: - UITableView DataSource and Delegate Methods
+extension NewsListVC: UITableViewDelegate, UITableViewDataSource {
+    
+    //numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    // cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TABLE_VIEW_CELL.NewsListCell.rawValue, for: indexPath) as? NewsListCell else { return UITableViewCell() }
+        return cell
+    }
 }
